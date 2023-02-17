c841326cf555cbc0cc8eaf51f6bbd78143d5246b
Harrison Akunne <harrisonderick.65@gmail.com>
2023-02-17 20:33:29 +0100

    chore: resolved scrolling issus with modal

--- src/components/TeamMember/TeamMember.jsx
+++ src/components/TeamMember/TeamMember.jsx
@@ -4,17 +4,25 @@ import "./TeamMember.css";
 
 const TeamMember = ({ name, position, image, bio }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-
+  const toggleModal = () => {
+    const mode = !modalIsOpen ? "open" : "closed";
+    setModalIsOpen(!modalIsOpen);
+    if (mode === "open") {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }
   return (
     <div>
-      <div className="team-member" onClick={() => setModalIsOpen(true)}>
+      <div className="team-member" onClick={toggleModal}>
         <img src={require('../../images/bg.jpg')} alt={name} />
         <h3>{name}</h3>
         <p>{position}</p>
       </div>
       <Modal
         isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
+        onRequestClose={toggleModal}
         contentLabel="Team Member Details"
         overlayClassName="modal-overlay"
         className="modal"
