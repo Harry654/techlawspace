144600e8e59216cbad214969f973df083b17e98e
Harrison Akunne <harrisonderick.65@gmail.com>
2023-02-27 13:29:37 +0100

    feat: added links for add member and upload article on dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Dashboard.css";
 import NavBar from "../../components/navBar/NavBar";
+import { MdOutlinePersonAdd } from "react-icons/md";
+import { HiDocumentArrowUp } from "react-icons/hi2";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-  
   return (
     <div className="dashboard">
       <NavBar />
-      <div className="dashboard-inner">
+      <div className="dashboard-links">
+        <Link to="/admin/add/member" className="link transition">
+          <MdOutlinePersonAdd size={50} color="rgb(80, 80, 250)" />
+        </Link>
+        <Link to="/admin/upload/article" className="link transition">
+          <HiDocumentArrowUp size={50} color="rgb(80, 80, 250)" />
+        </Link>
       </div>
     </div>
   );
